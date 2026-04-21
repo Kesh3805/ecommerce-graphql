64b7cecd38ae137ced8181ac7b2501f6d8d698c7
Kesh3805 <kesh.71772218121@gct.ac.in>
2026-04-21 16:00:09 +0530

    chore: remove obsolete migration scripts and documentation files

--- tmp-es-latency.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-console.log('start');
-const { Client } = require('@elastic/elasticsearch');
-console.log('client lib loaded');
-const client = new Client({ node: 'http://52.175.247.13:9200', auth: { username: process.env.ELASTICSEARCH_USERNAME, password: process.env.ELASTICSEARCH_PASSWORD } });
-(async()=>{ try { const r=await client.count({index:'products_detail_v1'}); console.log('count',r.count); } catch(e){ console.error('err',e); process.exit(1);} })();
